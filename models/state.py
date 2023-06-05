a8c69caa2b40fe9755e48a5d7a19643127a318d8
Ezea Richard <rclancing@gmail.com>
2023-06-05 19:00:21 +0100

    update

--- models/state.py
+++ models/state.py
@@ -9,23 +9,22 @@ class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
 
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', cascade='all, delete', backref='state')
+    else:
+        name = ''
 
-if getenv("HBNB_TYPE_STORAGE") == "db":
-    name = Column(String(128), nullable=False)
-    cities = relationship('City', cascade='all, delete', backref='state')
-else:
-    name = ''
-
-    @property
-    def cities(self):
-        """
-        Returns the join of city and state
-        """
-        from models import storage
-        all_cities = storage.all(City)
-        city = []
-        city_obj = all_cities.values()
-        for obj in city_obj:
-            if obj.state_id == self.id:
-                city.append(obj)
-        return city
+        @property
+        def cities(self):
+            """
+            Returns the join of city and state
+            """
+            from models import storage
+            all_cities = storage.all(City)
+            city = []
+            city_obj = all_cities.values()
+            for obj in city_obj:
+                if obj.state_id == self.id:
+                    city.append(obj)
+            return city
