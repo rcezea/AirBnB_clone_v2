abb6fa37906dea1bee95cbf8c7d4c56c96136d14
Ezea Richard <rclancing@gmail.com>
2023-06-05 18:57:59 +0100

    update

--- models/state.py
+++ models/state.py
@@ -8,12 +8,14 @@ from models.city import City
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
 
 
 if getenv("HBNB_TYPE_STORAGE") == "db":
+    name = Column(String(128), nullable=False)
     cities = relationship('City', cascade='all, delete', backref='state')
 else:
+    name = ''
+
     @property
     def cities(self):
         """
