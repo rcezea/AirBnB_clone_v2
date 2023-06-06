9db0f99cc50624309c29995f6c4dac0d40c5e502
Ezea Richard <rclancing@gmail.com>
2023-06-06 03:35:11 +0100

    update

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,9 +2,11 @@
 """
 Start a flask web application
 """
+
 from models import storage
 from flask import Flask, render_template
 app = Flask(__name__)
+
 
 @app.route('/states_list', strict_slashes=False)
 def states():
@@ -13,7 +15,9 @@ def states():
     """
     from models.state import State
     states = storage.all(State)
+    print(states)
     return render_template('7-states_list.html', states=states)
+
 
 @app.teardown_appcontext
 def teardown(exc):
@@ -21,6 +25,7 @@ def teardown(exc):
     Remove current SQLAlchemy Session
     """
     storage.close()
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', '5000')
